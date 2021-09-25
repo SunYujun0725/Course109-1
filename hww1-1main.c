d2eb0b7a10e245c60ee34b9791e82045ec533d3a
SunYujun0725 <91317039+SunYujun0725@users.noreply.github.com>
2021-09-25 23:10:26 +0800

    Arithmetic

---
+++ hww1-1main.c
@@ -0,0 +1,24 @@
+//
+//  main.c
+//  HW W1-1
+//
+//  Created by 孫渝鈞 on 2020/9/15.
+//  Copyright © 2020 孫渝鈞. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main()
+{
+    int x,y ;
+    
+    scanf("%d %d", &x,&y);
+    printf("%d\n",x+y);
+    printf("%d\n",x-y);
+    printf("%d\n",x*y);
+    printf("%d\n",x/y);
+    printf("%d\n",x%y);
+    
+    return 0;
+    
+}
